a6b4e6bb662203622a92f952a7526f7fffffb759
Alyona <noodlenickels@gmail.com>
2022-05-25 21:48:04 +0300

    button_change

--- src/pages/startButton.jsx
+++ src/pages/startButton.jsx
@@ -13,7 +13,7 @@ class startButton extends React.Component {
         return(this.props.status ?
             <div className="card-container middle">
                 <div className="spravka-text">Гадание таро - это приложение, в котором вам будет гадать голосовой помощник.</div>
-                <div className="spravka-text">Для того, чтобы начать гадание, нужно нажать на кнопку "Погадать" или сказать ассистенту "погадай мне/сделай расклад".</div>
+                <div className="spravka-text">Для того, чтобы начать гадание, нужно нажать на кнопку "Погадать" или сказать ассистенту "погадать/погадай мне/сделай расклад".</div>
                 <div className="spravka-text">После этого вам будет разложен ряд карт таро с описанием. Чтобы прослушать его через ассистента скажите "объясни значение".</div>
                 {/* <div className="big-text" style={{animation: 'fadeIn', 'animation-duration': '4s'}}>Нажмите в любом месте или скажите "Погадай мне", чтобы получить ваш персональный расклад</div> */}
             </div>
